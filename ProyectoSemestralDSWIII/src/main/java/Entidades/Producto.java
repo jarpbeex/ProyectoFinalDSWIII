57756a31b0d203b46cefd49e3e0859c1963dbf6a
ItsSher27 <140360720+ItsSher27@users.noreply.github.com>
2023-07-25 15:52:42 -0500

    Add files via upload

---
+++ ProyectoSemestralDSWIII/src/main/java/Entidades/Producto.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author laure
+ */
+public class Producto {
+    private String nombre;
+    private int codigoBarras;
+    private double precioEstandar;
+    private double precioDescuento;
+    
+    private String getNombre(){
+        return nombre;
+    }
+    
+    private void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    private int getCodigoBarras(){
+        return codigoBarras;
+    }
+    
+    private void setCodigoBarras(int codigoBarras){
+        this.codigoBarras = codigoBarras;
+    }
+    
+    private double getPrecioEstandar(){
+        return precioEstandar;
+    }
+    
+    private void setPrecioEstandar(double precioEstandar){
+        this.precioEstandar = precioEstandar;
+    }
+    
+    private double getPrecioDescuento(){
+        return precioDescuento;
+    }
+    
+    private void setPrecioDescuento(double precioDescuento){
+        this.precioDescuento = precioDescuento;
+    }
+}
+
+
